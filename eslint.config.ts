af42b2f9cbd417790f01ad936fac0dd132339e12
Vishnu c <vishnumalbro124@gmail.com>
2026-07-31 22:26:09 +0530

    feat(profile): implement profile ui and basic personal details

---
+++ eslint.config.ts
@@ -0,0 +1,16 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    extends: [js.configs.recommended],
+  },
+
+  ...tseslint.configs.recommended,
+]);
